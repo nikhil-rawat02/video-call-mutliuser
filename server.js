14f3dcf51049bfd681d6d98ceaffaf698a475442
nikhil rawat <nikhilrawat00@gmail.com>
2024-05-22 23:33:06 +0530

    update enter point of url

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -14,10 +16,35 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
 
-app.get("/", (req, rsp) => {
+app.get("/", (req, res) => {
   console.log('req', req)
-  // rsp.redirect(`/${req.caseId}`);
-  rsp.redirect(`/${uuidv4()}`);
+
+  const caseId = req.query.caseId;
+  const userId = req.query.userId;
+
+  // check in db
+  /**
+   * db schema will be like caseId , roomId, users (only allowed users)
+   * caseId and users should be update while booking appointment (user id is required to restrict the user)
+   * if when user comes here if caseId is not present return appropriate response
+   * if user Id is not present return appropriate response in session details
+   * if rooId is present return room id
+   * if room id is not present create new room Id store in db and return room id
+   */
+
+  if (!caseId) {
+    res.status(200).send("Case Id Not Found");
+    return;
+  }
+  if (!userId) {
+    res.status(200).send("User Id Not Found");
+    return;
+  }
+  
+  const roomId = caseId; // here roomId should be updated as per db response
+
+  res.redirect(`/${roomId}`);
+  
 });
 
 app.get("/:room", (req, res) => {
@@ -27,7 +54,7 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
+    // socket.to(roomId).broadcast.emit("user-connected", userId);
 
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message);
@@ -35,4 +62,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+server.listen(process.env.PORT || 3030, () => {
+  console.log(`server running on PORT ${process.env.PORT || 3030}`)
+});
